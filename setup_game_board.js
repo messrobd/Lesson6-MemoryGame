948fd5c6dd0cadf4faa36d9cefb59cb8362fbe58
Rob <robmessenger@iMac.lan>
2018-04-17 16:41:04 +0200

    feat: shuffle deck before dealing

--- setup_game_board.js
+++ setup_game_board.js
@@ -12,10 +12,22 @@ createEmptyBoard();
 var cards = ["A", "B", "C", "D", "E", "F", "G", "H"];
 var deck = ["A", "B", "C", "D", "E", "F", "G", "H", "A", "B", "C", "D", "E", "F", "G", "H"];
 
+function shuffleDeck(deck) {
+  var shuffledDeck = [];
+  var cardNumber, card;
+  while (deck.length > 0) {
+    cardNumber = Math.floor(Math.random() * deck.length);
+    card = deck.splice(cardNumber,1).toString();
+    shuffledDeck.push(card);
+  }
+  return shuffledDeck;
+}
+
 function dealCards(deck) {
+  var shuffledDeck = shuffleDeck(deck);
   var boardCards = $(board).children();
   boardCards.each(function(){
-    var card = deck.pop();
+    var card = shuffledDeck.pop();
     $(this).text(card);
   })
 }
