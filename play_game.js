968de224b5c659620540196e93499892825deb0b
Rob <robmessenger@iMac.lan>
2018-04-18 13:06:06 +0200

    feat: update game state from click handler

--- play_game.js
+++ play_game.js
@@ -1,6 +1,20 @@
 $('#board').on('click', '.card.back', function(event){
   var cardBack = event.target;
-  var cardFace = $(cardBack).siblings('.card.face')
+  var cardFace = $(cardBack).siblings('.card.face');
   $(cardFace).toggleClass('down', false);
   $(cardBack).toggleClass('down', true);
+  var card = $(cardFace).text();
+  updateGame(card);
 });
+
+var turnCard, cardMatch;
+var flipCount = 0;
+function updateGame(card) {
+  if (flipCount === 0) {
+    turnCard = card;
+    flipCount ++;
+  } else {
+    cardMatch = turnCard === card;
+    flipCount = 0;
+  }
+}
