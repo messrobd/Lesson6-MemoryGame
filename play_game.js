05d902daf431edecabf7211a5152cfb454cb8fac
Rob <robmessenger@iMac.lan>
2018-04-18 17:43:57 +0200

    feat: evaluate turns and reset board

--- play_game.js
+++ play_game.js
@@ -1,20 +1,48 @@
-$('#board').on('click', '.card.back', function(event){
-  var cardBack = event.target;
-  var cardFace = $(cardBack).siblings('.card.face');
+function flipCardUp(card) {
+  var cardFace = $(card).find('.card.face');
+  var cardBack = $(card).find('.card.back');
   $(cardFace).toggleClass('down', false);
   $(cardBack).toggleClass('down', true);
-  var card = $(cardFace).text();
-  updateGame(card);
-});
+}
 
-var turnCard, cardMatch;
-var flipCount = 0;
-function updateGame(card) {
-  if (flipCount === 0) {
-    turnCard = card;
-    flipCount ++;
-  } else {
-    cardMatch = turnCard === card;
-    flipCount = 0;
+function flipCardDown(card) {
+  var cardFace = $(card).find('.card.face');
+  var cardBack = $(card).find('.card.back');
+  $(cardFace).toggleClass('down', true);
+  $(cardBack).toggleClass('down', false);
+}
+
+function resetBoard(turn) {
+  turn.forEach(function(card) {
+    console.log("flipped");
+    flipCardDown(card);
+  });
+}
+
+function evaluateTurn(turn) {
+  var cardsPerTurn = 2;
+  if (turn.length === cardsPerTurn) {
+    var cardValues = [];
+    console.log(cardValues);
+    turn.forEach(function(card) {
+      var cardFace = $(card).find('.card.face');
+      var cardValue =  $(cardFace).text();
+      cardValues.push(cardValue);
+    });
+    if (cardValues[0] !== cardValues[1]) {
+      resetBoard(turn);
+      turn.length = 0;
+    }
   }
+}
+
+function startGame() {
+  var turn = [];
+  $('#board').on('click', '.card.back', function(event){
+    var card = $(event.target).parent();
+    flipCardUp(card);
+    turn.push(card);
+    console.log(turn);
+    evaluateTurn(turn);
+  });
 }
