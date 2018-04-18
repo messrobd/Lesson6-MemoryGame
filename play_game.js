a260f3d3e2186cc60d607ef6cb0bae43212e055d
Rob <robmessenger@iMac.lan>
2018-04-18 09:16:48 +0200

    feat: handler on card back to hide on click

--- play_game.js
+++ play_game.js
@@ -1,8 +1,6 @@
-var cards = $('.card');
-console.log(cards);
-cards.click(function(event){
+$('#board').on('click', '.card.back', function(event){
   var card = event.target;
-  console.log(event);
-  target.toggleClass('faceDown', false);
-  target.toggleClass('faceUp', true);
+  console.log(event.target);
+  $(card).toggleClass('down', true);
+  //target.toggleClass('faceUp', true);
 });
