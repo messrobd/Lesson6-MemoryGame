8940e949bfe91a336f6a350fb7980e247cf2bdcc
Rob <robmessenger@iMac.lan>
2018-05-03 13:01:33 +0200

    newGame action deals cards

--- game_state_machine.js
+++ game_state_machine.js
@@ -1,3 +1,38 @@
+function shuffleDeck(deck) {
+  var shuffledDeck = [];
+  var cardNumber, card;
+  while (deck.length > 0) {
+    cardNumber = Math.floor(Math.random() * deck.length);
+    card = deck.splice(cardNumber,1).toString();
+    shuffledDeck.push(card);
+  }
+  return shuffledDeck;
+}
+
+function flipCardUp(card) {
+  var cardFace = $(card).find('.card.face');
+  var cardBack = $(card).find('.card.back');
+  $(cardFace).toggleClass('down', false);
+  $(cardBack).toggleClass('down', true);
+}
+
+function flipCardDown(card) {
+  var cardFace = $(card).find('.card.face');
+  var cardBack = $(card).find('.card.back');
+  $(cardFace).toggleClass('down', true);
+  $(cardBack).toggleClass('down', false);
+}
+
+function dealCards(deck) {
+  var shuffledDeck = shuffleDeck(deck);
+  var boardCards = $('.card.face');
+  boardCards.each(function(){
+    var card = shuffledDeck.pop();
+    flipCardDown($(this).parent());
+    $(this).text(card);
+  });
+}
+
 const machine = {
   dispatch(actionName, ...payload) {
     const action = this.transitions[this.state][actionName];
@@ -13,7 +48,8 @@ const machine = {
   transitions: {
     'idle': {
       newGame: function () {
-        console.log('newGame action dispatched')
+        let deck = ["A", "B", "C", "D", "E", "F", "G", "H", "A", "B", "C", "D", "E", "F", "G", "H"];
+        dealCards(deck);
         this.changeStateTo('readyToPlay');
         //this.dispatch('play');
       }
