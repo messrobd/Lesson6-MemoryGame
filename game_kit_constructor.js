4ed21cffa0ab46c756b534dcfa1717a33f80e12b
Rob <robmessenger@iMac.lan>
2018-05-10 12:53:15 +0200

    style: define gameplay functions on gameKit

--- game_kit_constructor.js
+++ game_kit_constructor.js
@@ -15,5 +15,36 @@ function GameKit() {
         '  <div class="card back"></div>' +
         '</div>');
     }
+  },
+  this.flipCardUp = function(card) {
+    let cardFace = $(card).find('.card.face');
+    let cardBack = $(card).find('.card.back');
+    $(cardFace).toggleClass('down', false);
+    $(cardBack).toggleClass('down', true);
+  },
+  this.flipCardDown = function(card) {
+    let cardFace = $(card).find('.card.face');
+    let cardBack = $(card).find('.card.back');
+    $(cardFace).toggleClass('down', true);
+    $(cardBack).toggleClass('down', false);
+  },
+  this.shuffleDeck = function(deck) {
+    let shuffledDeck = [];
+    let cardNumber, card;
+    while (deck.length > 0) {
+      cardNumber = Math.floor(Math.random() * deck.length);
+      card = deck.splice(cardNumber,1).toString();
+      shuffledDeck.push(card);
+    }
+    return shuffledDeck;
+  },
+  this.dealCards = function(deck) {
+    let shuffledDeck = shuffleDeck(deck);
+    let boardCards = $('.card.face');
+    boardCards.each(function(){
+      let card = shuffledDeck.pop();
+      flipCardDown($(this).parent());
+      $(this).text(card);
+    });
   }
 }
