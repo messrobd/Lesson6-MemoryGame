816e66b59967947c0a2c2005c047c1abc68ecae9
Rob <robmessenger@iMac.lan>
2018-05-15 18:35:05 +0200

    style: prepare functions to streamline gameMachine

--- game_context.js
+++ game_context.js
@@ -1,7 +1,31 @@
 /*
-@description Properties of the game-in-progress
+@description Properties and behaviour of the game-in-progress
 */
 const gameContext = {
   turn: [],
   score: 0,
+  incrementTurn: function(card) {
+    this.push(card);
+  },
+  incrementScore: function() {
+    this.score ++;
+  },
+  /*turnCardsMatch: function() {
+    let card1 = $(this.turn[0]).find('.card.face').text(),
+        card2 = $(this.turn[1]).find('.card.face').text();
+    return card1 === card2;
+  },*/
+  resetTurn: function() {
+    this.turn.length = 0;
+  },
+  tryAgain: function() {
+    this.turn.forEach(function(card) {
+      gameBoard.flipCardDown(card);
+    });
+    this.resetTurn();
+  },
+  resetGameContext: function() {
+    this.resetTurn();
+    this.score = 0;
+  }
 }
